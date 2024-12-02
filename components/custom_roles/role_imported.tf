7e1a1970420eec8951dab9dd44f94f8e21055104
Marty Fox <martin.fox1@justice.gov.uk>
2024-12-02 15:30:20 +0000

    tf fmt

--- components/custom_roles/role_imported.tf
+++ components/custom_roles/role_imported.tf
@@ -1,26 +1,26 @@
 locals {
-    imported_role_definitions = yamldecode(file("${path.cwd}/role_definitions.yaml"))
+  imported_role_definitions = yamldecode(file("${path.cwd}/role_definitions.yaml"))
 }
 
 import {
-    for_each = { for role in local.imported_role_definitions : role.name => role }
+  for_each = { for role in local.imported_role_definitions : role.name => role }
 
-    id = each.value.id
-    to = azurerm_role_definition.imported_custom_roles[each.key]
+  id = each.value.id
+  to = azurerm_role_definition.imported_custom_roles[each.key]
 }
 
 resource "azurerm_role_definition" "imported_custom_roles" {
-    for_each = { for role in local.imported_role_definitions : role.name => role }
+  for_each = { for role in local.imported_role_definitions : role.name => role }
 
-    name              = each.key
-    description       = each.value.description
-    scope             = each.value.scope
-    assignable_scopes = each.value.assignableScopes
+  name              = each.key
+  description       = each.value.description
+  scope             = each.value.scope
+  assignable_scopes = each.value.assignableScopes
 
-    permissions {
-        actions          = each.value.permissions.actions
-        not_actions      = each.value.permissions.not_actions
-        data_actions     = each.value.permissions.data_actions
-        not_data_actions = each.value.permissions.not_data_actions
-    }
+  permissions {
+    actions          = each.value.permissions.actions
+    not_actions      = each.value.permissions.not_actions
+    data_actions     = each.value.permissions.data_actions
+    not_data_actions = each.value.permissions.not_data_actions
+  }
 }
